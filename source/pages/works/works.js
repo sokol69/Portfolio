b7147e613848d0006ff0c1be8da0b782fcbaccec
sokol69 <sokolov333d@gmail.com>
2018-01-03 11:02:24 +0300

    6th commit

--- source/pages/works/works.js
+++ source/pages/works/works.js
@@ -1,7 +1,9 @@
 import 'normalize.css';
 import './works.scss';
 import './style.css';
+import './../base.scss';
 import { EPROTONOSUPPORT } from 'constants';
+import { setTimeout } from 'timers';
 
 console.log('in works.js');
 
@@ -9,27 +11,31 @@ let btnPrev = document.getElementById('btn__prev');
 let btnNext = document.getElementById('btn__next');
 let sliderList = document.getElementById('slider-list');
 let position = 0;
+let flag = false;
 
-
-btnPrev.addEventListener('click', e => {
-  e.preventDefault();
-  console.log('click previous');
-
-  if (position > 0) {
-    position -= 100;
+let moveSlide = incr => {
+  if (!flag) {
+    flag = true;
+    position += incr;
+    if (position < 0) {
+      position = 300;
+    } else if (position > 300){
+      position = 0;
+    }
+    sliderList.style.left = '-' + position + '%';
   }
-  sliderList.style.left = '-' + position + '%';
-  return position;
+  setTimeout(() => {
+    flag = false;
+    return position, flag;
+  }, 700);
+};
+
+btnPrev.addEventListener('click', () => {
+  console.log('click previous');
+  moveSlide(-100);
 });
 
-btnNext.addEventListener('click', e => {
-  e.preventDefault();
+btnNext.addEventListener('click', () => {
   console.log('click next');
-  
-  position += 100;
-  if (position > 300) {
-    position = 0;
-  }
-  sliderList.style.left = '-' + position + '%';
-  return position;
+  moveSlide(100);
 });
